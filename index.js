e2a1a0a30873f4e79e30c79211af505f3dece658
zahidhasan2806 <jahidh714@gmail.comm>
2021-12-20 18:22:55 +0600

    service add and img upload

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const fileUpload = require('express-fileupload')
+
 
 //middleaware
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xohwd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -26,6 +29,36 @@ async function run() {
         const usersCollection = database.collection('users');
         const reviewsCollection = database.collection("reviews")
 
+
+        //POST API for  new products
+        app.post("/services", async (req, res) => {
+            const name = req.body.name;
+            const desc = req.body.desc;
+            const time = req.body.time;
+            const phone = req.body.phone;
+            const price = req.body.price;
+            const picture1 = req.files.picture1;
+            const picture2 = req.files.picture2;
+            const picture1Data = picture1.data;
+            const picture2Data = picture2.data;
+            const encodedData1 = picture1Data.toString('base64')
+            const encodedData2 = picture2Data.toString('base64')
+            const imgBuffer1 = Buffer.from(encodedData1, 'base64')
+            const imgBuffer2 = Buffer.from(encodedData2, 'base64')
+            const data = {
+                name, desc, price, time, phone,
+                image1: imgBuffer1, image2: imgBuffer2
+            }
+            const result = await serviceCollection.insertOne(data);
+            res.json(result);
+        })
+
+
+
+
+
+
+
         //GET API for all the services showing UI
         app.get("/services", async (req, res) => {
             const result = serviceCollection.find({});
@@ -36,8 +69,16 @@ async function run() {
         app.get("/services/:id", async (req, res) => {
             const serviceDetails = await serviceCollection.findOne({ _id: ObjectId(req.params.id) });
             res.send(serviceDetails);
-
         })
+
+
+        //Delete API- delete products
+        app.delete('/services/:id', async (req, res) => {
+            const deletedService = await serviceCollection.deleteOne({ _id: ObjectId(req.params.id) });
+            res.json(deletedService)
+        });
+
+
         //POST API for Products order
         app.post('/orders', async (req, res) => {
             const orders = await ordersCollection.insertOne(req.body);
